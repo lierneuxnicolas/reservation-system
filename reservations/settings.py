1ffcab12888403f5f15ec4a6a32c13e85354ca07
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-01-20 14:35:16 +0100

    Configure MySQL database and apply migrations

--- reservations/settings.py
+++ reservations/settings.py
@@ -52,10 +52,18 @@ WSGI_APPLICATION = 'reservations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'USER': 'root',
+        'PASSWORD': 'Pa$$w0rd',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
