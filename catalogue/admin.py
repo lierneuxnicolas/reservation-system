60f16e4e8338acd0caecb8e3c1824ce00a2997ea
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-01-25 19:48:38 +0100

    Backend authentication

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,24 +1,30 @@
 from django.contrib import admin
 from catalogue.models import Artist
 
+
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("id", "firstname", "lastname")
     search_fields = ("firstname", "lastname")
 
+
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from catalogue.models import UserMeta
+
 # Register your models here.
+
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
 class UserMetaInline(admin.StackedInline):
     model = UserMeta
     can_delete = False
     verbose_name_plural = "user_meta"
+
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = [UserMetaInline]
+
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- reservations/urls.py
+++ reservations/urls.py
@@ -1,11 +1,19 @@
+
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('catalogue/', include('catalogue.urls')),
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('admin/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('admin/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('catalogue.urls')),
 ]
 
-admin.site.index_title = "Projet Réservations"
-admin.site.index_header = "Projet Réservations HEADER"
-admin.site.site_title = "Spectacles"
+admin.site.index_title = "Gestion des réservations"
+admin.site.index_header = "Administration du site de réservation"
+admin.site.site_title = "Backoffice Réservations"
