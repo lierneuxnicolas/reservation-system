751a53536bb212377168f1d916e8632fa50cec14
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-01-26 14:00:05 +0100

    Frontend authentication – Password recovery

--- reservations/settings.py
+++ reservations/settings.py
@@ -81,5 +81,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+#...
 LOGIN_URL = '/accounts/login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+# Email configuration
+# https://docs.djangoproject.com/fr/5.2/topics/email/#smtp-backend
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
