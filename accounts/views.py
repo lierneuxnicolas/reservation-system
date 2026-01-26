6e48fb42b50d059f9c54ae903d65c1b32d44c134
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-01-26 14:44:41 +0100

    Frontend authentication  Advanced signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,18 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect
 from django.contrib import messages
+from .forms import UserSignUpForm
 
 class UserSignUpView(UserPassesTestMixin, CreateView):
-    form_class = UserCreationForm
+    form_class = UserSignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
     def test_func(self):
-        return self.request.user.is_anonymous or self.request.user.is_superuser
+        return self.request.user.is_anonymous or \
+               self.request.user.is_superuser
 
     def handle_no_permission(self):
         messages.error(self.request, "Vous êtes déjà inscrit!")
